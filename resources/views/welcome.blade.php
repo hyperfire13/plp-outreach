d2a73b97d4d9864640c4e3233a6f8ec705d87d1c
kenneth <kybanez53@gmail.com>
2026-09-10 21:25:07 +0800

    added logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="/images/logo.png">
+        <title>{{ config('app.name', 'PLP Outreach') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
